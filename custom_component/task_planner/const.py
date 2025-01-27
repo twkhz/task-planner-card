391b41b708198a1bd2c57bb6bcd2d740b6149e06
twkhz <twistermcneal@gmail.com>
2025-01-27 15:29:44 +0100

    Create const.py

---
+++ custom_component/task_planner/const.py
@@ -0,0 +1,19 @@
+"""Constants for Task Planner integration."""
+from homeassistant.const import CONF_NAME, CONF_ICON
+
+DOMAIN = "task_planner"
+DEFAULT_TITLE = "Task Planner"
+DEFAULT_ICON = "mdi:checkbox-marked-circle"
+
+# Configuration keys
+CONF_USERS = "users"
+CONF_TASKS = "tasks"
+CONF_ROLE = "role"
+CONF_DAYS = "days"
+CONF_COLOR = "color"
+CONF_DESCRIPTION = "description"
+
+# Input text configuration
+CONF_MIN = "min"
+CONF_MAX = "max"
+CONF_MODE = "mode"
